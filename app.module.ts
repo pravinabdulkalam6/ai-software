a0bc0ebd2193025d4e404b94b01c989b3639b4c3
pravin <pravinabdulkalam6@gmail.com>
2023-11-06 15:51:47 +0530

    my commit 2

--- app.module.ts
+++ app.module.ts
@@ -3,27 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CheckboxModule } from 'primeng/checkbox';
-import { FormsModule } from '@angular/forms';
-import { SharedmoduleModule } from './sharedmodule/sharedmodule.module';
-import { EmployeeComponent } from './employee/employee.component';
-import { StaffComponent } from './staff/staff.component';
 import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
-    StaffComponent,
     CardComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CheckboxModule,
-    FormsModule,
-    SharedmoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
